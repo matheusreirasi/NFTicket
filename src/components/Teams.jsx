145e9ac3f398ab47b19a4d3081c9a931c4ed1471
Matheus Felipe <matheus.reirasi@gmail.com>
2022-11-15 01:05:36 -0300

    Teams

---
+++ src/components/Teams.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Teams = ()=> {
+    return(
+        <div className="flex flex-col mx-20">
+            <div  className="text-2xl py-7 px-14">
+                Sinistros
+            </div>
+            <div className="text-2xl py-7 px-14">
+                Piratas
+            </div>
+        </div>
+    )
+}
+
+export default Teams
