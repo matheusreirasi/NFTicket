b02df30299c94d1a380e41b9e4d7ce6c35f4e3a2
Matheus Felipe <matheus.reirasi@gmail.com>
2022-11-27 23:49:37 -0300

    background light

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import {BsShieldFillCheck} from "react-icons/bs"
+import {AiFillThunderbolt} from "react-icons/ai"
+
+const ServiceCard = ({color, title, icon}) => {
+    <div className="flex items-center text-xl px-12 hover:shadow-xl">
+        <div className="p-5 rounded-md">
+            {icon}
+        </div>
+    </div>
+}
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="flex items-center w-fit h-fit">
+            <div className="flex items-center text-xl ml-12 pr-12 rounded-xl hover:shadow-2xl hover:bg-fuchsia-200 hover:bg-opacity-20 hover:shadow-purple-600">
+                <div className="white-glassmorphism p-3">
+                    <div className="bg-[#9B19B8] p-2 rounded-full">
+                        {<BsShieldFillCheck fontSize={29} className="text-white"/>}
+                    </div>
+                </div>
+                <h1 className="px-6 text-white">
+                    Rápido
+                </h1>
+            </div>
+            <div className="flex items-center text-xl ml-12 pr-12 rounded-xl hover:shadow-2xl hover:bg-orange-200 hover:bg-opacity-20 hover:shadow-orange-300">
+                <div className="white-glassmorphism p-3">
+                    <div className="bg-[#F34213] p-2 rounded-full">
+                        {<AiFillThunderbolt fontSize={29} className="text-white"/>}
+                    </div>
+                </div>
+                <h1 className="px-6 text-white">
+                    Seguro
+                </h1>
+            </div>
+            <ServiceCard 
+                color="bg-[#9B19B8]"
+                title="Rápido"
+                icon={BsShieldFillCheck}
+            />
         </div>
     )
 }
