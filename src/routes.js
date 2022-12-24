1e676a89cc3ff9b857ad945627a10a4f8cfaa53c
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-24 20:54:34 -0300

    page routes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home"
 import Tickets from "./pages/Tickets"
 
+import {AnimatePresence} from "framer-motion"
+
+const RoutesApp = () => {
+  const location = useLocation()
+  return (
+      <AnimatePresence>
+        <Routes key={location.pathname} location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/tickets" element={<Tickets />} />
+        </Routes>
+      </AnimatePresence>
+  )
+}
 
 
-const Router = createBrowserRouter([
-  {
-    path:"/",
-    element:<Home/>
-  },
-  {
-    path:"/tickets",
-    element:<Tickets/>
-  }
-])
-
-export default Router
+export default RoutesApp
