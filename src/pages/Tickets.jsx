1172c5e08a99efd2238fdb3d4e4c1a164b434883
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-24 21:26:07 -0300

    website tickets page

--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Teams , Purchase} from '../components/indexComponents'
+import { motion } from 'framer-motion'
 
 const Tickets = () => {
   return (
-    <div>Tickets</div>
+    <motion.main
+      initial={{width:"0"}}
+      animate={{width:"100vw"}}
+      exit={{x:"100vw", transition:{duration:0.2}}}
+    >
+      <main className='flex items-center my-8 mx-16'>
+        <Teams/>
+        <Purchase/>
+      </main>
+    </motion.main>
   )
 }
 
