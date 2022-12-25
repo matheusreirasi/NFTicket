2ff5c063c4dafc11a76cdff7b1dd057eb9f73ddd
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-24 21:17:10 -0300

    change html tag <a> to react-router-dom <link>

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom"
 import React from "react"
 
 
-const Welcome = () => {
+const Banner = () => {
     return (
         <div className="w-full h-[30vh] flex justify-between items-center px-14 bg-cover bg-predio bg-center opacity-90">
-            <div className="text-5xl font-semibold text-white">
-                NFTicket
-            </div>
+            <Link to="/">
+                <div className="text-5xl font-semibold text-white">
+                    NFTicket
+                </div>
+            </Link>
         </div>
     )
 }
 
-export default Welcome
+export default Banner
