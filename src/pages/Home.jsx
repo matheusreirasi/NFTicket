d40f95c32330f80e347366bdfc33eabe5e745a41
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-22 20:20:07 -0300

    funcionando bem em telas de pc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
 import React from "react"
-import {Welcome, Teams, Footer} from "../components/indexComponents"
-
+import {Welcome, Teams, Footer} from "../components/indexComponents" 
 import { motion } from "framer-motion"
 
 const Home = () => {
-  let screenSizeWidth = window.innerWidth
+
+  const screenSizeWidth = window.innerWidth
   
   return (
     <motion.div
-    initial={{width:"100vw"}}
-    animate={{width:"-100vw"}}
-    exit={{x:"0", transition:{duration:0.2}}}
+    initial={{width:"100%"}}
+    animate={{width:"100vw"}}
+    exit={{x:"0", transition:{duration:0.4}}}
     >
-      <main className={`flex items-center mx-28`}>
+      <main className={`flex justify-around content-center ${screenSizeWidth<770 ? "flex-col content-center" : ""}`}>
         <Welcome />
         <Teams />
       </main>
-      <footer>
+      <footer className={`ml-12 ${screenSizeWidth<770 ? "flex content-center justify-around ml-0" : ""}`}>
         <Footer />
       </footer>
     </motion.div>
--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -3,13 +3,16 @@ import { Teams , Purchase} from '../components/indexComponents'
 import { motion } from 'framer-motion'
 
 const Tickets = () => {
+
+  const screenSizeWidth = window.innerWidth
+  
   return (
     <motion.main
-      initial={{width:"0"}}
-      animate={{width:"100vw"}}
-      exit={{x:"100vw", transition:{duration:0.2}}}
+      initial={{width:"0%"}}
+      animate={{width:"100%"}}
+      exit={{x:"100%", transition:{duration:0.3}}}
     >
-      <main className='flex items-center mx-16'>
+      <main className={`flex justify-around content-center ${screenSizeWidth<770 ? "flex flex-col justify-center mx-4" : ""}`}>
         <Teams/>
         <Purchase/>
       </main>
