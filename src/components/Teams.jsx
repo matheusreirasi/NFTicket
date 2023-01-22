016cb5cf45ab5214199802f6c9acfb462ef2a13b
matheusreirasi <matheus.reirasi@gmail.com>
2023-01-22 20:30:10 -0300

    removed some useless values

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -4,12 +4,15 @@ import sinistros from "../images/sinistros.png"
 import piratas from "../images/piratas.jpg"
 
 const Teams = ()=> {
+
+    const screenSizeWidth = window.innerWidth
+
     return(
-        <div className="flex flex-col mx-12 justify-around items-center">
+        <div className={`flex flex-col items-center mx-12 my-5 ${screenSizeWidth<770 ? "mx-0" :""}`}>
             <Link to="/tickets">
                 <div className="flex items-center text-3xl my-5 mx-14 cursor-pointer">
-                    <img src={sinistros} alt="img" width={125}className="rounded-full  bg-auto bg-no-repeat bg-center"/>
-                    <div className="px-12 text-white">
+                    <img src={sinistros} alt="img" width={125} className="rounded-full"/>
+                    <div className="px-10 text-white">
                         Sinistros
                     </div>
                 </div>
@@ -17,7 +20,7 @@ const Teams = ()=> {
             <Link to="/tickets">
                 <div className="flex items-center text-3xl my-5 mx-14 cursor-pointer">
                     <img src={piratas} alt="img" width={125} className="rounded-full"/>
-                    <div className="px-12 text-white">
+                    <div className="px-10 text-white">
                         Piratas
                     </div>
                 </div>
